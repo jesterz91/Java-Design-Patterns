d9c01130c38240ba4c709a32fdf864a613408c3d
jesterz91 <jesterz91@naver.com>
2018-09-20 23:38:22 +0900

    싱글톤 구현방법 추가

--- src/Singleton/Korea.java
+++ src/Singleton/Korea.java
@@ -3,6 +3,11 @@ package Singleton;
 /**
  * 싱글톤 패턴(Singleton Pattern)
  * 오직 하나만 생성해야할 객체가 필요할 때 사용하는 패턴
+ *
+ * President        : synchronized 키워드 이용
+ * PrimeMinister    : 정적 초기화 시 생성
+ * ChiefJustice     : Double-checking Locking
+ * Speaker          : enum 이용
  */
 
 public class Korea {
@@ -11,9 +16,26 @@ public class Korea {
         President president1 = President.getInstance();
         President president2 = President.getInstance();
 
+        PrimeMinister primeMinister1 = PrimeMinister.getInstance();
+        PrimeMinister primeMinister2 = PrimeMinister.getInstance();
+
+        ChiefJustice chiefJustice1 = ChiefJustice.getInstance();
+        ChiefJustice chiefJustice2 = ChiefJustice.getInstance();
+
+        Speaker speaker1 = Speaker.INSTANCE;
+        Speaker speaker2 = Speaker.getInstance();
+
         // 오직 하나의 인스턴스만 생성되기 때문에 동일한 값 출력
         System.out.println(president1);
         System.out.println(president2);
 
+        System.out.println(primeMinister1);
+        System.out.println(primeMinister2);
+
+        System.out.println(chiefJustice1);
+        System.out.println(chiefJustice2);
+
+        System.out.println(speaker1);
+        System.out.println(speaker2);
     }
 }
